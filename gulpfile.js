b2fe57e63cdc1d7fd62c892d22c114ac0f1d4064
xiaoc <1740454645@qq.com>
2018-12-17 10:12:19 +0800

    跳转页完成

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,26 @@
 var gulp = require('gulp');
 var server = require('gulp-webserver');
+var fs = require('fs');
+var path = require('path');
+var url = require('url');
 gulp.task('server', function() {
     return gulp.src('./src')
         .pipe(server({
-            port: "9090"
+            port: "9090",
+            proxies: [{
+                source: '/api/get/train_tickets',
+                target: 'http://localhost:9090/api/get/train_tickets'
+            }],
+            middleware: function(req, res, next) {
+                if (req.url === '/favicon.ico') {
+                    return res.end();
+                }
+                var pathname = url.parse(req.url).pathname;
+                if (pathname === '/') {
+                    res.end(fs.readFileSync(path.join(__dirname, 'index.html')));
+                } else {
+
+                }
+            }
         }))
 })
